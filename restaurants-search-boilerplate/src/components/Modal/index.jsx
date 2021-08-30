d5b9c78756c4a4333d0bacbf3a9657da20a23381
dmssantos <dms.santos_@hotmail.com>
2021-08-30 20:49:46 -0300

    create modal

---
+++ restaurants-search-boilerplate/src/components/Modal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Portal from './Portal';
+
+const Modal = () => {
+  return (
+    <Portal>
+      <div>asdfasfd</div>
+    </Portal>
+  );
+};
+
+export default Modal;
